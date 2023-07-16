24579931bd6be594bb3617abbb6edf4a579b1b46
Rosaka12 <92648981+Rosaka12@users.noreply.github.com>
2023-07-16 13:06:09 +0300

    deleted unnecessary files

--- watchlink.py
+++ watchlink.py
@@ -1,15 +1,52 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session, abort, request
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_client import OAuth
 
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.app_context().push()
 app.config['SECRET_KEY'] = '2f23a88461aa5335f22200988f1ece8e1dcd731f58f1bb13e0dadd98b5485ad4'
-# the following line means that database will be created in project directory
+
+# Database, the following line means that database will be created in project directory
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///watchlink.db'
 db = SQLAlchemy(app)
+
+oauth = OAuth(app)
+
+
+google = oauth.register(
+    name='google',
+    client_id='1028720449984-bulqio8hbjc4mej8910g9oi1tlk93hn5.apps.googleusercontent.com',
+    client_secret='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',
+    authorize_params={
+        'scope': 'openid email profile',
+        'access_type': 'offline'
+    },
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile',
+        'token_endpoint_auth_method': 'client_secret_basic',
+        'token_placement': 'header'
+    },
+    issuer='https://accounts.google.com'
+)
+
+CLIENT_ID_FACEBOOK = 294032019865535
+CLIENT_SECRET_FACEBOOK = "9228e7da8d1cc3097ae9f9664d5ee22f"
+
+
+facebook = oauth.register(
+    'facebook',
+    consumer_key=CLIENT_ID_FACEBOOK,
+    consumer_secret=CLIENT_SECRET_FACEBOOK,
+    request_token_params={'scope': 'email'},
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth'
+)
 
 
 class User(db.Model):
@@ -35,7 +72,6 @@ class Video(db.Model):
         return f"Video ('{self.title}', '{self.date_posted}', '{self.image_file}')"
 
 
-
 simple_data = [
     {
         'videoName': 'name1',
@@ -59,7 +95,9 @@ simple_data = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", videos=simple_data)
+    email = dict(session)['profile']['email']
+    return f'Hello, you are logged in as {email}!'
+#    return render_template("home.html", videos=simple_data)
 
 
 @app.route("/about")
@@ -67,6 +105,7 @@ def about():
     return render_template("about.html")
 
 
+# register account in watchlink
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -75,15 +114,66 @@ def register():
         return redirect(url_for('home'))
     return render_template("sign up.html", title='Register', form=form)
 
+
+# native app login
 @app.route("/login")
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Logged in with account {form.username.data}!', 'success')
+        flash(f'Logged in with account {form.email.data}!', 'success')
         return redirect(url_for('home'))
     return render_template("sign in.html", title='Login', form=form)
 
 
+# google login
+@app.route('/google_login')
+def google_login():
+    redirect_uri = url_for('google_authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def google_authorize():
+    google = oauth.create_client('google')
+    try:
+        token = google.authorize_access_token()
+    except Exception as e:
+        print("Error obtaining access token:", e)
+
+    resp = google.get('https://www.googleapis.com/oauth2/v3/userinfo')
+    user_info = resp.json()
+    session['email'] = user_info['email']
+    session['profile'] = user_info
+    session.permanent = True
+    return redirect('/')
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+# to do
+@app.route("/facebook_login")
+def facebook_login():
+    return redirect(url_for('facebook_authorized', callback=url_for('facebook_authorized', _external=True)))
+
+
+# to do
+@app.route('/facebook-authorized')
+def facebook_authorized():
+    response = facebook.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return 'Access denied: reason={}, error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+
+    session['facebook_token'] = (response['access_token'], '')
+
+    return redirect(url_for('/'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
--- website/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-@views.route('/home')
-def home():
-    return "<h1>Home</h1>"
-    #return render_template("home.html")
