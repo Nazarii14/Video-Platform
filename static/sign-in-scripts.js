42fcb6a6982ca07a1702a5dbefc895b43d72108b
Rosaka12 <92648981+Rosaka12@users.noreply.github.com>
2023-07-18 12:26:01 +0300

    register system is done

--- static/sign-in-scripts.js
+++
@@ -1,6 +0,0 @@
-const signUpButton = document.getElementById("signUp");
-signUpButton.addEventListener('click', signUpButtonClick);
-function signUpButtonClick() {
-    window.location.href = "SIGN UP.html";
-}
-
