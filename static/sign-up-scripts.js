cce926f96c1a6cb78b07339b2ebdddc0b1c75176
VolodiaTutko <volodatutko32.com>
2023-07-25 00:18:31 +0300

    add Profile Page

--- static/sign-up-scripts.js
+++
@@ -1,6 +0,0 @@
-const signInButton = document.getElementById("signIn");
-signInButton.addEventListener('click', signInButtonClick);
-function signInButtonClick() {
-    window.location.href = "SIGN IN.html";
-}
-
