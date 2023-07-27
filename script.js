1e3767d626a7616b5686b9e383b50ceb5cb2c01a
WeaRD276-01 <oleksandr.zhenchenko@lnu.edu.ua>
2023-07-27 19:25:20 +0300

    Added sidebar

--- script.js
+++ script.js
@@ -1,56 +1,32 @@
 const clear_icon = document.querySelector(".clear");
 
-clear_icon.onclick = () =>
+const icon_sidebar = document.getElementById('menu-button');
+const sidebar = document.getElementById('sidebar');
+const close_sidebar = document.getElementById('close-sidebar');
+
+icon_sidebar.onclick = () =>
 {
-  document.getElementById("search").value = "";
+  sidebar.classList.toggle('active');
 };
 
-
-
-var i=0;
-
-setInterval(()=>{
-
-    document.getElementById("Animated-container").style.background="linear-gradient(45deg, rgba(49,226,215,1) 0%, rgba(253,29,199,0.4) "+i*1+"%, darkorchid 100%)";
-    document.getElementById("Animated-container").style.backgroundRepeat="no-repeat";
-    
-    
-    
-     i++;
-     
-     
-    if(i==100){
-        
-        i=0;      
-               
-    }
+close_sidebar.onclick = () => 
+{
    
-    
-},100);
-
-JavaScript
-const messageBox = document.querySelector(".message-box");
-const userIcon = document.querySelector(".user-icon");
-
-let timer;
-
-userIcon.onclick = () => {
-  messageBox.classList.toggle('active');
-  if (messageBox.classList.contains('active')) {
-    clearTimeout(timer);
-  } else {
-    timer = setTimeout(() => {
-      messageBox.classList.remove('active');
-    }, 6000);
-  }
+    if (sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active');
+    };
+    icon_sidebar.style.visibility = "visible";
 };
 
-messageBox.addEventListener('mouseenter', () => {
-  clearTimeout(timer);
-});
+var i = 0;
+var increment = 1;
 
-messageBox.addEventListener('mouseleave', () => {
-  timer = setTimeout(() => {
-    messageBox.classList.remove('active');
-  }, 200);
-});
+setInterval(() => {
+  document.getElementById("Animated-container").style.background = "linear-gradient(45deg, rgba(49,226,215,1) 0%, rgba(253,29,199,0.4) " + i + "%, darkorchid 100%)";
+
+  i += increment;
+
+  if (i >= 100 || i <= 0) {
+    increment *= -1;
+  }
+}, 100);
